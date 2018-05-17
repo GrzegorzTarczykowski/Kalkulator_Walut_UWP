a053c1bcad6a6a06f338858500fbae9041bff6d1
GrzegorzTarczykowski <grzegorz.tarczykowski@gmail.com>
2018-05-17 20:13:08 +0200

    commit

--- kalkulator_walut_tarczykowski/kalkulator_walut_tarczykowski/MainPage.xaml.cs
+++ kalkulator_walut_tarczykowski/kalkulator_walut_tarczykowski/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -30,6 +31,29 @@ namespace kalkulator_walut_tarczykowski
         {
             this.InitializeComponent();
         }
+        private void zmianaWaluty()
+        {
+            //odczyt wczytanej kwoty
+            // konwersja na liczbe
+            //przeliczenie z waluty na PLN oraz z PLN na waluete docelowa
+            double kwota;
+            if (double.TryParse(txtKwota.Text, out kwota))
+            {
+                var zWaluty = lbxZWaluty.SelectedItem as PozycjaTabeliA;
+                var naWalute = lbxNaWalute.SelectedItem as PozycjaTabeliA;
+                string sepAkt = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator;
+                double kurs;
+                double.TryParse(zWaluty.kurs_sredni.Replace(",", sepAkt), out kurs);
+                double kwotaPrzeliczona = kwota * kurs;
+                double.TryParse(naWalute.kurs_sredni.Replace(",", sepAkt), out kurs);
+                kwotaPrzeliczona = kwotaPrzeliczona / kurs;
+                tbPrzeliczona.Text = string.Format("{0:F2} {1}", kwotaPrzeliczona, naWalute.kod_waluty);
+            }
+            else
+            {
+                tbPrzeliczona.Text = "";
+            }
+        }
 
         private async void Grid_Loaded(object sender, RoutedEventArgs e)
         {
@@ -43,7 +67,7 @@ namespace kalkulator_walut_tarczykowski
             {
                 throw;
             }
-            if(dane!="")
+            if (dane != "")
             {
                 kursyAktualne.Clear();
                 XDocument daneKursowe = XDocument.Parse(dane);
@@ -58,16 +82,38 @@ namespace kalkulator_walut_tarczykowski
             kursyAktualne.Insert(0, new PozycjaTabeliA() { kurs_sredni = "1,0000", kod_waluty = "PLN", przelicznik = "1" });
             lbxZWaluty.ItemsSource = kursyAktualne;
             lbxNaWalute.ItemsSource = kursyAktualne;
-            lbxZWaluty.SelectedIndex = 0;
-            lbxNaWalute.SelectedIndex = 0;
+            string klucz = lbxZWaluty.Name;
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey(klucz))
+            {
+                lbxZWaluty.SelectedIndex = (int)ApplicationData.Current.LocalSettings.Values[klucz];
+            }
+            else
+            {
+                lbxZWaluty.SelectedIndex = 0;
+            }
+            klucz = lbxNaWalute.Name;
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey(klucz))
+            {
+                lbxNaWalute.SelectedIndex = (int)ApplicationData.Current.LocalSettings.Values[klucz];
+            }
+            else
+            {
+                lbxNaWalute.SelectedIndex = 0;
+            }
         }
 
         private void txtKwota_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //odczyt wczytanej kwoty
-            // konwersja na liczbe
-            //przeliczenie z waluty na PLN oraz z PLN na waluete docelowa
-            PozycjaTabeliA zWaluty = kursyAktualne[lbxZWaluty.SelectedIndex];
+            zmianaWaluty();
+        }
+
+        private void lbxZWaluty_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            zmianaWaluty();
+            var listBox = sender as ListBox;
+            int co = listBox.SelectedIndex;
+            string gdzie = listBox.Name;
+            ApplicationData.Current.LocalSettings.Values[gdzie] = co;
         }
     }
 }
